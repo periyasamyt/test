cf695cc89da043e1ad44ca162d15f621ae42c1df
perit20 <periyasamy.thirumalaivelsamy@outlook.com>
2020-03-01 20:21:36 +0530

    Fix

--- WebApplication26/Controllers/HomeController.cs
+++ WebApplication26/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApplication26.Models;
+using Test.Models;
 
 namespace WebApplication26.Controllers
 {
